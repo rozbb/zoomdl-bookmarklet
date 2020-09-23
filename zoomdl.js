d761072c05311d2c6bfd684a84ba7ddd62635de5
Michael Rosenberg <michael@mrosenberg.pub>
2020-09-23 01:16:55 -0400

    Bookmarklet now re-enables right-click since Zoom has disabled it by default

--- zoomdl.js
+++ zoomdl.js
@@ -29,4 +29,65 @@ javascript:(function(){
     // Insert the link just above the video
     var container = document.getElementsByClassName("main")[0];
     container.prepend(download_link);
+
+    // Zoom disabled right-click on their website. Everything below is just to re-enable it. Thanks
+    // to Chema for their answer on SuperUser
+    // https://superuser.com/questions/279166/how-do-i-set-google-chrome-to-not-allow-javascript-to-hijack-my-right-click-menu
+
+    function enableContextMenu(aggressive = false) {
+        void(document.ondragstart = null);
+        void(document.onselectstart = null);
+        void(document.onclick = null);
+        void(document.onmousedown = null);
+        void(document.onmouseup = null);
+        void(document.body.oncontextmenu = null);
+        enableRightClickLight(document);
+        if (aggressive) {
+            enableRightClick(document);
+            removeContextMenuOnAll("body");
+            removeContextMenuOnAll("img");
+            removeContextMenuOnAll("td");
+        }
+    }
+
+    function removeContextMenuOnAll(tagName) {
+        var elements = document.getElementsByTagName(tagName);
+        for (var i = 0; i < elements.length; i++) {
+            enableRightClick(elements[i]);
+        }
+    }
+
+    function enableRightClickLight(el) {
+        el || (el = document);
+        el.addEventListener("contextmenu", bringBackDefault, true);
+    }
+
+    function enableRightClick(el) {
+        el || (el = document);
+        el.addEventListener("contextmenu", bringBackDefault, true);
+        el.addEventListener("dragstart", bringBackDefault, true);
+        el.addEventListener("selectstart", bringBackDefault, true);
+        el.addEventListener("click", bringBackDefault, true);
+        el.addEventListener("mousedown", bringBackDefault, true);
+        el.addEventListener("mouseup", bringBackDefault, true);
+    }
+
+    function restoreRightClick(el) {
+        el || (el = document);
+        el.removeEventListener("contextmenu", bringBackDefault, true);
+        el.removeEventListener("dragstart", bringBackDefault, true);
+        el.removeEventListener("selectstart", bringBackDefault, true);
+        el.removeEventListener("click", bringBackDefault, true);
+        el.removeEventListener("mousedown", bringBackDefault, true);
+        el.removeEventListener("mouseup", bringBackDefault, true);
+    }
+
+    function bringBackDefault(event) {
+        event.returnValue = true;
+        (typeof event.stopPropagation === 'function') && event.stopPropagation();
+        (typeof event.cancelBubble === 'function') && event.cancelBubble();
+    }
+
+    // Enable right click
+    enableContextMenu();
 })();
